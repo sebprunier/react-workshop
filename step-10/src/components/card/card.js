f235dfb8914a7726b3a1a6292ae66c0f0dab16c1
Sébastien Prunier <sebastien.prunier@gmail.com>
2015-12-14 23:28:23 +0100

    Amélioration du rendu des Cards

--- step-10/src/components/card/card.js
+++ step-10/src/components/card/card.js
@@ -2,9 +2,9 @@ var React = require('react');
 
 var cardStyle = {
     border: "1px grey solid",
-    padding: 5,
-    marginTop: 8,
-    marginBottom: 8
+    padding: '0px 16px 16px 16px',
+    marginBottom: 16,
+    boxShadow: '0px 1px 6px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.24)'
 };
 
 var Card = React.createClass({
