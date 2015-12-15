d7a2e5cfa143cb79310512f02eee13225fee4268
Sébastien Prunier <sebastien.prunier@gmail.com>
2015-12-15 15:02:11 +0100

    Ajout de la route permettant de modifier le statut d'une tâche

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,8 @@ var id1 = uuid.v4();
 var id2 = uuid.v4();
 var id3 = uuid.v4();
 
+var statuses = ["NEW", "IN_PROGRESS", "DONE"];
+
 var todosById = {};
 todosById[id1] = {id: id1, text: "Ceci est un todo", status: "NEW"};
 todosById[id2] = {id: id2, text: "Ceci est un autre todo", status: "IN_PROGRESS"};
@@ -42,7 +44,7 @@ server.get('/todos', function (req, res, next) {
 server.get('/todos/:id', function (req, res, next) {
     var todo = todosById[req.params.id];
     if (!todo) {
-        return next(new restify.NotFoundError("Not Found"))
+        return next(new restify.NotFoundError("Not Found"));
     }
     res.send(todo);
     return next();
@@ -57,6 +59,19 @@ server.post('/todos', function (req, res, next) {
     };
     todosById[newTodoId] = newTodo;
     res.send(201, newTodo);
+});
+
+server.put('/todos/:id/status', function(req, res, next) {
+    var todo = todosById[req.params.id];
+    if (!todo) {
+        return next(new restify.NotFoundError("Not Found"));
+    }
+    var newStatus = req.body.status;
+    if (!newStatus || statuses.indexOf(newStatus) < 0) {
+        return next(new restify.BadRequestError("Payload not valid"));
+    }
+    todo.status = newStatus;
+    res.send(200, todo);
 });
 
 
